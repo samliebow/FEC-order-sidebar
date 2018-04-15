3d6ee3d6819e4e9802a738544d4c250a6ad8f579
samliebow <sam.liebow@gmail.com>
2018-04-15 13:20:20 -0700

    Favorite: add unstyled boxes with simple toggle functionality

--- client/src/Favorite.jsx
+++ client/src/Favorite.jsx
@@ -1,5 +1,61 @@
 import React from 'react';
 
-const Favorite = (props) => (<div className="mainItem" id="favorite">You like it</div>);
+class Favorite extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorited: false,
+      added: false,
+      userLists: [],
+    };
+    this.toggleFavorite = this.toggleFavorite.bind(this);
+    this.addToList = this.addToList.bind(this);    
+  }
+
+  toggleFavorite() {
+    this.setState({
+      favorited: !this.state.favorited,
+    });
+  }
+
+  addToList() {
+    // Will add actual functionality beyond toggling later
+    this.setState({
+      added: !this.state.added,
+    });
+  }
+
+  renderFavorite() {
+    return (
+      <span id="favorite-button" onClick={this.toggleFavorite}>
+        {this.state.favorited ?
+          '[rHrt] Favorited' :
+          '[gHrt] Favorite'
+        }
+      </span>
+    );
+  }
+
+  renderAdded() {
+    return (
+      <span id="add-to-button" onClick={this.addToList}> 
+        {this.state.added ?
+          '[chk] Added' :
+          '[noChk] Add to'
+        }
+      </span>
+    );
+  }
+
+  render() {
+    return (
+      <div className="mainItem" id="favorite-component">
+        {this.renderFavorite()}
+        &nbsp;
+        {this.renderAdded()}
+      </div>
+    );
+  }
+}
 
 export default Favorite;
