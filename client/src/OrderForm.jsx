22b5fead14dec170b504011fdcbf32e7ffde9b96
samliebow <sam.liebow@gmail.com>
2018-04-14 21:04:12 -0700

    OrderForm: Add functionless buy + cart buttons, HCed special message

--- client/src/OrderForm.jsx
+++ client/src/OrderForm.jsx
@@ -15,18 +15,37 @@ class OrderForm extends React.Component {
     this.quantityOptions = makeQuantityOptions(this.props.data.quantity);
   }
 
+  renderSpecialMessage() {
+    const specialMessage = 'others want'; // Hard-coding for now
+    if (specialMessage === 'others want') {
+      return (
+        <div>
+          <span>((Image will go here)) </span>
+          {/* The real thing is vector graphics in a <g> tag,
+          will have to figure out how to replicate. */}
+          <span><b>Other people want this.</b> 4 people have this in their carts right now.</span>
+        </div>
+      );
+    } else {
+      return <br />;
+    }
+  }
+
   render() {
     return (
       <div className="mainItem" id="order-form">
         <h4>{this.props.data.title}</h4>
+
         <div id="price-and-question">
           <span id="price">$7.29+ [HC]</span>
           <span><button>Ask a question</button></span>
         </div>
+
         <div id="variations">
           {this.props.data.variationTypes.map(type => (
             <div className="variation">
               <div>{type}</div>
+
               <select>
                 {this.props.data.variations[type]
                   .map((variation) => {
@@ -39,16 +58,34 @@ class OrderForm extends React.Component {
                   })
                 }
               </select>
+
             </div>
           ))}
         </div>
+
         <div id="quantity">
           <div>Quantity</div>
           <select>
             {this.quantityOptions}
           </select>
         </div>
-      </div>);
+
+        <div id="buy-now">
+          <button>
+          Buy it now {'>'}
+          </button>
+        </div>
+
+        <div id="add-cart">
+          <button>
+          Add to cart
+          </button>
+        </div>
+
+        {this.renderSpecialMessage()}
+
+      </div>
+    );
   }
 }
 
