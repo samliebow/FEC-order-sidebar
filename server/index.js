7dd17814575889a28cb4ff84401bff8ae7abdae2
samliebow <sam.liebow@gmail.com>
2018-04-16 10:13:48 -0700

    Update server to serve static assets on listing pages instead of /

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./public'));
+// matches listings/<any nine digits>/<any-letter-and-dash-sequence>/<not ending with 'data'>
+app.use(/listings\/[0-9]{9}\/[A-z-]+\/(?!(data))/, express.static(path.join(__dirname, '../public')));
 
 app.listen(541, () => console.log('Listening on port 541...'));
