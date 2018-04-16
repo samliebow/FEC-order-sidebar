20ec1e54462f416ec097a971ba830d142ad719aa
samliebow <sam.liebow@gmail.com>
2018-04-16 12:27:38 -0700

    Look up listing in DB upon GET, modify DB setup/seeding to accommodate

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const Listing = require('../data/Listing.js');
 
 const app = express();
 
 // matches listings/<any nine digits>/<any-letter-and-dash-sequence>/<not ending with 'data'>
 app.use(/listings\/[0-9]{9}\/[A-z-]+\/(?!(data))/, express.static(path.join(__dirname, '../public')));
 
-app.listen(541, () => console.log('Listening on port 541...'));
+app.get(
+  'listings/:listingNum/:listingName/data',
+  (req, res) => {
+    const { listingNum, listingName } = req.params;
+    Listing.findOne({ listingNum, listingName })
+      .then(result => res.send(JSON.stringify(result)));
+  },
+);
+
+mongoose.connect('mongodb://localhost/etsy');
+mongoose.connection.on('error', err => console.error(`Database connection error: ${err}`));
+mongoose.connection.once('open', () => {
+  console.log('Connection to database successful!');
+
+  app.listen(541, () => console.log('Listening on port 541...'));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: 'source-map',
   mode: 'development',
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx', '.json'],
   },
   module: {
     rules: [
