370a12c01e52e36bc06cefe3a9c6e511f2a7ab0e
samliebow <sam.liebow@gmail.com>
2018-04-16 14:25:01 -0700

    Cleanup and minor bug fixing in fake data generation

--- data/makeFakeIds.js
+++ data/makeFakeIds.js
@@ -5,7 +5,7 @@ const ids = [];
 for (let i = 0; i < 100; i += 1) {
   const id = {};
   id.listingNum = `${Math.round(Math.random() * 1000000000)}`.padStart(9, '0');
-  id.listingName = `${faker.commerce.productAdjective()}-${faker.commerce.productMaterial()}-${faker.commerce.product()}-${faker.commerce.color()}`.toLowerCase();
+  id.listingName = `${faker.commerce.productAdjective()}-${faker.commerce.productMaterial()}-${faker.commerce.product()}-${faker.commerce.color().replace(' ','-')}`.toLowerCase();
   ids.push(id);
 }
 
